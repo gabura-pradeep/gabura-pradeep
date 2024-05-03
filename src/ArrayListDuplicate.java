34a1231441571e4b24bd5f94c7693aad488baa7f
pradeep-pers <pradeep.pintu@gmail.com>
2024-05-03 19:53:12 +0530

    ArrayList is added

---
+++ src/ArrayListDuplicate.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayListDuplicate {
+
+	public static void main(String[] args) {
+		int arr[]= {2,2,3,6,7,6,7,8,9,8,9};
+		
+		Set<Integer>num=new HashSet<Integer>();
+		System.out.println("________________________________________________");
+		
+		for(Integer num1 :arr)
+		{
+			if(num.add(num1)==false)
+			{
+				System.out.println(" Dupilcate Numbers Are:" +num1);
+			}
+		}
+     System.out.println(" Print the array Without Duplicate:" +num);
+	}
+
+
+
+	}
+
+
